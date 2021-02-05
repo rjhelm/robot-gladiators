fae6b17fc18d79608781bb963a703f02fa2d1a49
rjhelm <77085453+rjhelm@users.noreply.github.com>
2021-02-04 17:46:24 -0700

    added console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,19 @@
-window.prompt("What is your robot's name?");
+var playerName = window.prompt("What is your robot's name?");
 
-function fight(); {
+window.alert(playerName);
+// Console Log
+console.log(playerName);
+
+console.log("This logs a string, good for leaving a message");
+
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+
+function fight() {
   window.alert("The fight has begun!");
-}
+};
+
+// fight ();
